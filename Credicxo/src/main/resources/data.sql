d863a1e565ca3eccc25a12bf996641aafe545150
prodBhaskerHR <prodBhaskerHR@gmail.com>
2020-10-04 17:02:27 +0530

    5 commitr

---
+++ Credicxo/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO question_entity (id,question,optionA,optionB,optionC,optionD,answer) VALUES
+  ('1','Ordinary table salt is sodium chloride. What is baking soda?','Potassium chloride','Potassium carbonate','Potassium hydroxide','Sodium bicarbonate','Sodium bicarbonate'),
+  ('2','Plants receive their nutrients mainly from?','chlorophyll','atmosphere','light','soil','soil'),
+  ('3','Movement of cell against concentration gradient is called?','osmosis','active transport','passive transport','diffusion','active transport'),
+  ('4','The percentage of irrigated land in India is about?','45','65','35','25','35'),
+  ('5','The southernmost point of peninsular India, that is, Kanyakumari, is?','north of Tropic of Cancer','south of the Equator','south of the Capricorn','north of the Equator','north of the Equator'),
+  ('6','Who invented the BALLPOINT PEN?','Biro Brothers','Waterman Brothers','Bicc Brothers','Write Brothers','Biro Brothers');
